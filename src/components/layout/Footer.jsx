de5733d9452b9bdf68b46d712a17a18c72ce259e
junhyuk2000 <wnscjcj2000@naver.com>
2026-02-28 19:25:46 +0900

    Modify footer message

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <div><a href="https://green-debt-775.notion.site/front-2797f54e231d80d68137d5325ef08c0c"><img src="/images/notion.svg" alt="" /></a></div>
     </div>
     <div>
-      <p className="footer_copy">© 2025. Choi Jun Hyuk. All rights reserved.</p>
+      <p className="footer_copy">© 2026. Choi Jun Hyuk. All rights reserved.</p>
     </div>
   </div>
   );
